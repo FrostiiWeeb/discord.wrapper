6a0e6724586e72b3aa3129f3ca8a3867f0a3f573
FrostiiWeeb <rayanhassansuja@gmail.com>
2021-04-08 12:21:25 +0200

    New

--- discord/wrapper/intents.py
+++ discord/wrapper/intents.py
@@ -7,8 +7,6 @@ class Intents:
 	Discord priveliged intents.
 
     """
-	
-<<<<<<< HEAD
 	@classmethod
 	def all(cls):
 		return 28671
@@ -19,13 +17,4 @@ class Intents:
 		
 	@classmethod
 	def presences(cls):
-=======
-	def all(self):
-		return 28671
-		
-	def members(self):
-		return 255
-		
-	def presences(self):
->>>>>>> e13b87682e850c26e831292bb19976027a2c79c2
 		return 32512
